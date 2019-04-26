da222229779622bcd7e22710bd306ac22038fb1b
Jeff-Gui <1964728070@qq.com>
2019-04-26 09:07:49 +0800

    Integrated task1~4 with UI
    Completely finished task1~4

--- Jeff/IBI_group_project.py
+++ Jeff/IBI_group_project.py
@@ -51,48 +51,145 @@ def dtp(x):
         print('Cannot find initiation codon!')
 #------------------------------------------------------------------------------
 """
+UI
 This recursion function provides a user-friendly console interface.
 The user can call the task funcitons for any times and exit whenever he wants.
 The function contains a error-detecting part to ensure the correct input.
 """
-dic = {1:GC_content, 2:cplmty, 3:mrna}
 seq = ''
-s = 1 # s controls whether the input of the task 1~4 is needed, if one input the wrong number at first, then the task should not be chosen before asking for a correct input
-def inpt():
+opt = ''
+opt2 = ''
+s = 0
+t = 0
+#-----------------------------MAIN FUNCTION------------------------------------
+def ui():
     global seq
+    global opt
+    global opt2
     global s
-    if not seq:
-        seq = input('Please input your sequence:\n')
-        while 1==1:    
-            try:
-                if re.search('[^ATCGU]', seq) or (seq.find('T') * seq.find('U') > -1):
-                    print('Please input the correct sequence:', end='') #end='' ensures no extra white line after input
-                    seq = input('')
-                else:
-                    break
-            except:
-                print('Please input the correct sequence:', end='')
-                seq = input('')
-    if s == 1: 
-        opt = input('Choose your task:\n [1]:GC content calculation [2]:complementary sequence [3]:mRNA [4]:peptide [0]:exit\n')
-    while 1==1:
-        try:
-            opt = int(opt)
-            if opt == 0:
-                seq = '' #reset the string for next time use
-                break
-            elif opt in dic.keys():
-                dic[opt](seq)
-                s = 1
-                inpt()
-                break
-            else:
-                print('Please input the correct number:', end='')
-                opt = input('')
-        except:
-            print('Please input the correct number:', end='')
-            opt = input('')
-            s = 0
-            inpt()
-            break
-#-----------------------
+    global t
+    print('Select your task:\n[1]:GC content calculation [2]:complementary sequence [3]:mRNA [4]:peptide [5]:hydropathy [0]:exit', end='')
+    opt = input('')
+    ckopt()
+    if t == 0:
+        print('Input your sequence:',end='')
+        seq = input('')
+        t = 1
+    if opt == 0:
+        s = 1
+        return
+    elif opt == 4:
+        ckmrna()
+        dtp(seq)
+    elif opt == 5:
+        print('Select your sequence type:\n[1]: protein sequence [2]mRNA sequence',end='')
+        opt2 = input('')
+        ckopt2()
+        if opt2==1:
+            ckpt()
+        else:
+            ckmrna()
+    elif opt in {1,2,3}:
+        ckdna()
+        if opt==1:
+            GC_content(seq)
+        elif opt==2:
+            cplmty(seq)
+        else:
+            mrna(seq)
+    print('Keep your sequence? Y/N',end='')
+    p = input('')
+    if p == 'n':
+        seq = ''
+        t = 0
+    return 
+#------------------------------------------------------------------------------
+def ckopt():
+    """
+    check the number input when selecting task
+    """
+    global opt
+    if re.search('[^123450]',opt):
+        print('Please input the correct number:',end='')
+        opt = input('')
+        if not ckopt():
+            return False
+    else:
+        opt = int(opt)
+    return True
+ 
+def ckopt2():
+    """
+    check the number input when selecting sequence type in task 5
+    """
+    global opt2
+    if re.search('[^12]',opt2):
+        print('Please input the correct number:',end='')
+        opt2 = input('')
+        if not ckopt2():
+            return False
+    else:
+        opt2 = int(opt2)
+    return True
+       
+def ckdna():
+    """
+    check DNA
+    maybe add into task 5 in the future?
+    """
+    global seq
+    if re.search('[^ATCG]', seq):
+        print('Please input the correct DNA sequence:', end='')
+        seq = input('')
+        if not ckdna():
+            return False
+    else:
+        return True
+    return True 
+
+def ckmrna():
+    """
+    check mRNA
+    assumptions:
+        1. if sequence length is not 3n, then it only contains one AUG (initial condon) and one stop condon
+        2. if sequence length is 3n, then it is 'part' of a whole peptide
+    """
+    global seq
+    p = re.findall('AUG[AUCG]*|AUG[AUCG]*|AUG[AUCG]*', seq) #only consider one initial condon and one stop condon
+    try:
+        seq = p[0]
+        seq2 = seq[::-1] #find the last pattern of "stop condon", if 3* length in between, then valid
+        if (len(seq)-seq2.find('GAU'))%3==0 or (len(seq)-seq2.find('AAU'))%3==0 or (len(seq)-seq2.find('AGU'))%3==0:
+            return True
+        else:
+            print('Please input the correct mRNA sequence:', end='')
+            seq = input('')
+            if not ckmrna():
+                return False
+    except:         #if sequence length is times of 3
+        if len(seq)%3==0 and not re.search('[^AUCG]',seq): #assume that sequence is meaningful(can be translated)
+            return True
+        else:
+            print('Please input the correct mRNA sequence:', end='')
+            seq = input('')
+            if not ckmrna():
+                return False
+    return True
+
+def ckpt():
+    """
+    check protein
+    """
+    global seq
+    if re.search('[^GAVLIMFWPSTCYNQDEKRH]',seq):
+        print('Please input the correct protein sequence:', end='')
+        seq = input('')
+        if not ckpt():
+            return False
+    else:
+        return True
+    return True
+
+#---------------------------EXECUTE--------------------------------------------
+while s==0:
+    ui()
