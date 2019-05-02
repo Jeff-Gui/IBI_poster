9733dd029e5ab81b7f4784bf19690410c83b7a82
Jeff-Gui <1964728070@qq.com>
2019-05-02 22:07:07 +0800

    task5 v1.2

--- Jeff/task5.py
+++ Jeff/task5.py
@@ -6,10 +6,9 @@ Created on Thu May  2 20:58:01 2019
 - Jeff
     现在可以完成的功能：
         输入指定单链DNA, 检索所有579个蛋白对应长度的dna片段与蛋白结合的概率
-        概率阈值设定在0.00001，超过则输出matrix号码，名称，对应dna片段
+        概率阈值设定在0.01，超过则输出matrix号码，名称，对应dna片段
         输出在answer list里
-    可能的改进：
-        用户指定阈值
+    可能的改进:
         输出dna片段的位置 或 可视化
 - Robin
 
@@ -27,7 +26,8 @@ nmlist = []
 data = open('JASPARdbs.txt').readlines()
 
 dic = {'A':0, 'C':1, 'G':2, 'T':3}
-seq = input('seq:\n')
+seq = input('Please input your sequence:\n')
+trd = float(input('Please input a threshold:\n'))
 answer = []
 for t in range(0, 579): #database has 2895 lines with 579 matrices
     nm = re.findall('\d+', data[5*t+1])    
@@ -48,7 +48,7 @@ for t in range(0, 579): #database has 2895 lines with 579 matrices
         for seg in seglist:
             for k in range(0, len(seg)):
                 psb = psb * a[dic[seg[k]], k]
-            if psb > 0.00001:          # determine whether possibility is over the threshold
+            if psb > trd:          # determine whether possibility is over the threshold
                 answer += [data[5*t] + ' ' + seg]
             psb = 1 #reset
                 
