bfcb8c68d5b070ab2769d6667b0e36579f2ddc22
Jeff-Gui <1964728070@qq.com>
2019-05-03 13:30:51 +0800

    task5 v2
    normalisation and pseudo-count

--- Jeff/task5.py
+++ Jeff/task5.py
@@ -4,15 +4,15 @@
 Created on Thu May  2 20:58:01 2019
 -------COMMENTS HERE--------
 - Jeff
-    现在可以完成的功能：
-        输入指定单链DNA, 检索所有579个蛋白对应长度的dna片段与蛋白结合的概率
-        概率阈值设定在0.01，超过则输出matrix号码，名称，对应dna片段
-        输出在answer list里
-    可能的改进:
-        输出dna片段的位置 或 可视化
+
 - Robin
 
 - Harper
+
+------SAMPLE HERE-------
+
+Please input your sequence:
+ATGCTTCAGAAAGGTCTTACG
         
 @author: IBI group1
 """
@@ -22,13 +22,18 @@ os.chdir('/Users/jefft/Desktop/ZJE/IBI(local)/git_repository/IBI_poster/Jeff') #
 
 import numpy as np
 import re
+import pandas as pd
+import math
 nmlist = []
+segment=[]
+position=[]
+possibility=[]
+score = []
 data = open('JASPARdbs.txt').readlines()
-
 dic = {'A':0, 'C':1, 'G':2, 'T':3}
-seq = input('Please input your sequence:\n')
-trd = float(input('Please input a threshold:\n'))
-answer = []
+seq = input('Input your sequence:\n')
+trd = float(input('Input a threshold:\n'))
+
 for t in range(0, 579): #database has 2895 lines with 579 matrices
     nm = re.findall('\d+', data[5*t+1])    
     col = int(len(nm)) #col is the length of dna segment
@@ -38,19 +43,33 @@ for t in range(0, 579): #database has 2895 lines with 579 matrices
         seglist.append(seq[s:s+col])
     #every possible segment of input dna is stored in seglist
     if seglist:
+        # generate PFM
         for i in range(5*t+1,5*t+5): # i : the number of matrix lines
             nm = re.findall('\d+', data[i])
             for j in range(0,col):
                 a[i-5*t-1,j] = int(nm[j])
+        # convert PFM to PWM
         add = a[0,0]+a[1,0]+a[2,0]+a[3,0] #count the number of sample DNA sequence
-        a = (1/add)*a
-        psb = 1 #possibility that segment can bind with protein
-        for seg in seglist:
-            for k in range(0, len(seg)):
-                psb = psb * a[dic[seg[k]], k]
-            if psb > trd:          # determine whether possibility is over the threshold
-                answer += [data[5*t] + ' ' + seg]
-            psb = 1 #reset
-                
-                
-        
+        psdc = add**0.5 #pseudocount
+        for m1 in range(0,4):
+            for m2 in range(0,col):
+                temp = a[m1,m2]
+                temp = (temp+psdc/4)/(add+psdc)
+                temp = math.log(temp/0.25,2)
+                a[m1,m2] = temp
+        sc = 0
+        for j in range(0, len(seglist)):
+            for k in range(0, col):
+                sc += a[dic[seglist[j][k]], k]
+        #determine whether the score is over the threshold
+            if sc > trd:
+                name=re.findall(r'\t(.+?)\n',data[5*t])[0]
+                score.append(sc)
+                nmlist.append(name)
+                segment.append(seglist[j])
+                p=str(j+1)+'-'+str(j+col)
+                position.append(p)
+            sc = 0 #reset
+df=pd.DataFrame({'position':position, 'segment':segment,'prot_name':nmlist,'score':score})                
+print(df)
+    
